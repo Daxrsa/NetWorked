4d67206a977f5d7f86aff6524e0561f84e13e719
Fitore <ttahirifitore@gmail.com>
2023-04-16 20:44:46 +0200

    Implemented Repository Pattern and added endpoints

--- backend/ProfileMatch.Service/src/ProfileMatch/API/Program.cs
+++ backend/ProfileMatch.Service/src/ProfileMatch/API/Program.cs
@@ -1,11 +1,16 @@
+using Application.Core.InterfaceRepos;
+using Application.Services;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using System.Windows.Input;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IRecommendationsRepo, RecommendationService>();
+builder.Services.AddScoped<IResultsRepo, ResultsService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ backend/ProfileMatch.Service/src/ProfileMatch/Application/Services/ResultsService.cs
@@ -0,0 +1,47 @@
+﻿using Application.Core.InterfaceRepos;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ResultsService : IResultsRepo
+    {
+        private readonly ProfileMatchDbContext _context;
+        public ResultsService(ProfileMatchDbContext context) 
+        {
+            _context= context;
+        }
+        public async Task<bool> Add(ProfileMatchingResult entity)
+        {
+            var results = _context.Results.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public Task<bool> Delete(ProfileMatchingResult entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ProfileMatchingResult>> GetAll()
+        {
+            return await _context.Results.ToListAsync();
+        }
+
+        public Task<IEnumerable<ProfileMatchingResult>> GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(ProfileMatchingResult entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
