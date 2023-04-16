3f564d45982df25605ebaf90779cba8ee06d379a
Fitore <ttahirifitore@gmail.com>
2023-04-16 18:38:39 +0200

    Models and Database setup for ProfileMatch service

---
+++ backend/ProfileMatch.Service/src/ProfileMatch/Persistance/Migrations/20230416160505_initialCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reccomendations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Result = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reccomendations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Result = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reccomendations");
+
+            migrationBuilder.DropTable(
+                name: "Results");
+        }
+    }
+}
---
+++ backend/ProfileMatch.Service/src/ProfileMatch/Persistance/ProfileMatchDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Persistence
+{
+    public class ProfileMatchDbContext : DbContext
+    {
+        public ProfileMatchDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<ProfileMatchingResult> Results { get; set; }
+        public DbSet<Reccomendation> Reccomendations { get; set; }
+    }
+}
