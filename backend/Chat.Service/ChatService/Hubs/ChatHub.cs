c302efc712d67730e86291ac644655358e92e8bd
Erosi1 <em52473@ubt-uni.net>
2023-05-07 22:07:51 +0200

    SendMessage method added

--- backend/Chat.Service/ChatService/Hubs/ChatHub.cs
+++ backend/Chat.Service/ChatService/Hubs/ChatHub.cs
@@ -5,15 +5,25 @@ namespace ChatService.Hubs
     public class ChatHub :Hub
     {
         private readonly string _botUser;
+         private readonly IDictionary<string, UserConnection> _connections;
         
-        public ChatHub()
+        public ChatHub(IDictionary<string, UserConnection> connections)
         {
             _botUser = "MyChat Bot";
+            _connections = connections;
         }
         public async Task JoinRoom(UserConnection userConnection)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
             await Clients.Group(userConnection.Room).SendAsync("RecieveMessage", _botUser,$"{userConnection.User}has joined {userConnection.Room}");
         }
+          public async Task SendMessage(string message)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", userConnection.User, message);
+            }
+        }
+
     }
 }
--- backend/Chat.Service/ChatService/Program.cs
+++ backend/Chat.Service/ChatService/Program.cs
@@ -7,13 +7,27 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+builder.Services.AddSingleton<IDictionary<string, UserConnection>>(opts => new Dictionary<string, UserConnection>());
 
 var app = builder.Build();
 
 app.UseRouting();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors();
 
-app.MapHub<ChatHub>("/chat");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<ChatHub>("/chat");
+});
 
 app.Run();
