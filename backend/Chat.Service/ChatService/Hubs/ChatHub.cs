843b469b764a79a7c0aeff5f77d48321af14680f
Erosi1 <em52473@ubt-uni.net>
2023-05-10 21:07:45 +0200

    Added List of Connected Users

--- backend/Chat.Service/ChatService/Hubs/ChatHub.cs
+++ backend/Chat.Service/ChatService/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Linq;
 
 namespace ChatService.Hubs
 {
@@ -20,11 +21,13 @@ namespace ChatService.Hubs
             {
                 _connections.Remove(Context.ConnectionId);
                 Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has left");
+                SendConnectedUsers(userConnection.Room);
 
             }
 
             return base.OnDisconnectedAsync(exception);
         }
+
         public async Task SendMessage(string message)
         {
             if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
@@ -40,9 +43,15 @@ namespace ChatService.Hubs
             await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
             _connections[Context.ConnectionId] = userConnection;
             await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has joined {userConnection.Room}");
+            await SendConnectedUsers(userConnection.Room);
+        }
+
+        public Task SendConnectedUsers(string room)
+        {
+            var users = _connections.Values.Where(c => c.Room == room).Select(c => c.User);
+            return Clients.Group(room).SendAsync("UsersInRoom", users);
         }
 
 
     }
 }
-
