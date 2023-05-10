8842bcc8bd8a7412dec7caf0dd8124a72eaecfb4
Erosi1 <em52473@ubt-uni.net>
2023-05-10 20:46:20 +0200

    Added functionality so user can see who left

--- backend/Chat.Service/ChatService/Hubs/ChatHub.cs
+++ backend/Chat.Service/ChatService/Hubs/ChatHub.cs
@@ -14,6 +14,17 @@ namespace ChatService.Hubs
             _connections = connections;
 
         }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                _connections.Remove(Context.ConnectionId);
+                Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has left");
+
+            }
+
+            return base.OnDisconnectedAsync(exception);
+        }
         public async Task SendMessage(string message)
         {
             if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
