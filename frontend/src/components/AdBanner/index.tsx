73f1dc89fa74042e21e1c63c9c1e8903b688e710
Erosi1 <em52473@ubt-uni.net>
2023-05-14 14:03:11 +0200

    Banner for ads added

---
+++ frontend/src/components/AdBanner/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const AdBanner: React.FC = () => {
+  return (
+    <Container className="ad-banner">
+      <span>NetWorked </span>
+     - The best social networking site designed specifically for the business community
+    </Container>
+  );
+};
+
+export default AdBanner;
