c18b001669244ea256de2dbd4b8c7526b4510ac6
Fitore <ttahirifitore@gmail.com>
2023-05-14 20:07:28 +0200

    Model created and tested

--- backend/Job.Service/Job/CompanyRecommender/Program.cs
+++ backend/Job.Service/Job/CompanyRecommender/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.ML.Data;
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System.Drawing;
 
 namespace CompanyRecommender
 {
@@ -16,7 +18,72 @@ namespace CompanyRecommender
         private static string ModelPath = GetAbsolutePath(ModelRelativePath);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Call the following piece of code for splitting the ratings.csv into ratings_train.csv and ratings.test.csv.
+           // Program.DataPrep();
+
+            //STEP 1: Create MLContext to be shared across the model creation workflow objects
+            MLContext mlContext = new MLContext();
+
+            //STEP 2: Read data from text file using TextLoader by defining the schema for reading the movie recommendation datasets and return dataview.
+            var trainingDataView = mlContext.Data.LoadFromTextFile<CompanyRating>(path: TrainingDataLocation, hasHeader: true, separatorChar: ',');
+
+            Console.WriteLine("=============== Reading Input Files ===============");
+            Console.WriteLine();
+
+            // ML.NET doesn't cache data set by default. Therefore, if one reads a data set from a file and accesses it many times, it can be slow due to
+            // expensive featurization and disk operations. When the considered data can fit into memory, a solution is to cache the data in memory. Caching is especially
+            // helpful when working with iterative algorithms which needs many data passes. Since SDCA is the case, we cache. Inserting a
+            // cache step in a pipeline is also possible, please see the construction of pipeline below.
+            trainingDataView = mlContext.Data.Cache(trainingDataView);
+
+            Console.WriteLine("=============== Transform Data And Preview ===============");
+            Console.WriteLine();
+
+            //STEP 4: Transform your data by encoding the two features userId and movieID.
+            //        These encoded features will be provided as input to FieldAwareFactorizationMachine learner
+            var dataProcessPipeline = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "userIdFeaturized", inputColumnName: nameof(CompanyRating.userId))
+                                          .Append(mlContext.Transforms.Text.FeaturizeText(outputColumnName: "companyIdFeaturized", inputColumnName: nameof(CompanyRating.companyId))
+                                          .Append(mlContext.Transforms.Concatenate("Features", "userIdFeaturized", "companyIdFeaturized")));
+
+            // STEP 5: Train the model fitting to the DataSet
+            Console.WriteLine("=============== Training the model ===============");
+            Console.WriteLine();
+            var trainingPipeLine = dataProcessPipeline.Append(mlContext.BinaryClassification.Trainers.FieldAwareFactorizationMachine(new string[] { "Features" }));
+            var model = trainingPipeLine.Fit(trainingDataView);
+
+            //STEP 6: Evaluate the model performance
+            Console.WriteLine("=============== Evaluating the model ===============");
+            Console.WriteLine();
+            var testDataView = mlContext.Data.LoadFromTextFile<CompanyRating>(path: TestDataLocation, hasHeader: true, separatorChar: ',');
+
+            var prediction = model.Transform(testDataView);
+
+
+            //STEP 7:  Try/test a single prediction by predicting a single movie rating for a specific user
+            Console.WriteLine("=============== Test a single prediction ===============");
+            Console.WriteLine();
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<CompanyRating, CompanyRatingPrediction>(model);
+            CompanyRating testData = new CompanyRating() { userId = "3", companyId = "2" };
+
+            var companyRatingPrediction = predictionEngine.Predict(testData);
+            Console.WriteLine($"UserId:{testData.userId} with companyd: {testData.companyId} Score:{Sigmoid(companyRatingPrediction.Score)} and Label {companyRatingPrediction.PredictedLabel}", Color.YellowGreen);
+            Console.WriteLine();
+
+            //STEP 8:  Save model to disk
+            Console.WriteLine("=============== Writing model to the disk ===============");
+            Console.WriteLine(); mlContext.Model.Save(model, trainingDataView.Schema, ModelPath);
+
+            Console.WriteLine("=============== Re-Loading model from the disk ===============");
+            Console.WriteLine();
+            ITransformer trainedModel;
+            using (FileStream stream = new FileStream(ModelPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                trainedModel = mlContext.Model.Load(stream, out var modelInputSchema);
+            }
+
+            Console.WriteLine("Press any key ...");
+            Console.Read();
+
         }
 
         public static void DataPrep()
