320c04a548485b4c0ae1254f230541902ab3e3f4
Daorsa Hyseni <79546481+Daxrsa@users.noreply.github.com>
2023-05-15 20:46:44 +0200

    /

--- frontend/src/components/MainChat.jsx
+++ frontend/src/components/MainChat.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react';
-import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
-import Lobby from './Lobby'
-import Chat from './Chat';
-import '../App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useState } from "react";
+import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
+import Lobby from "./Lobby";
+import Chat from "./Chat";
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const MainChat = () => {
   const [connection, setConnection] = useState();
   const [messages, setMessages] = useState([]);
-  const [users,setUsers]=useState([]);
-  
+  const [users, setUsers] = useState([]);
 
   const joinRoom = async (user, room) => {
     try {
@@ -19,19 +17,18 @@ const MainChat = () => {
         .configureLogging(LogLevel.Information)
         .build();
 
-        connection.on("UsersInRoom",(users)=>{
-          setUsers(users);
-        })
+      connection.on("UsersInRoom", (users) => {
+        setUsers(users);
+      });
 
       connection.on("ReceiveMessage", (user, message) => {
-        setMessages(messages => [...messages, { user, message }]);
+        setMessages((messages) => [...messages, { user, message }]);
       });
-connection.onclose(e=>{
-  setConnection();
-  setMessages([]);
-  setUsers([]);
-})
-     
+      connection.onclose((e) => {
+        setConnection();
+        setMessages([]);
+        setUsers([]);
+      });
 
       await connection.start();
       await connection.invoke("JoinRoom", { user, room });
@@ -39,14 +36,14 @@ connection.onclose(e=>{
     } catch (e) {
       console.log(e);
     }
-  }
+  };
   const closeConnection = async () => {
     try {
-        await connection.stop();
-    }catch(e){
+      await connection.stop();
+    } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   const sendMessage = async (message) => {
     try {
@@ -54,17 +51,24 @@ connection.onclose(e=>{
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
-
-  return <div className='app' >
-    
-    <h2 className='h2c'>NetWorked</h2>
-    <hr className='line' />
-    {!connection
-      ? <Lobby joinRoom={joinRoom} />
-      : <Chat sendMessage={sendMessage} messages={messages} closeConnection={closeConnection} users={users}/>}
-  </div>
-}
+  return (
+    <div className="app">
+      <h2 className="h2c">NetWorked</h2>
+      <hr className="line" />
+      {!connection ? (
+        <Lobby joinRoom={joinRoom} />
+      ) : (
+        <Chat
+          sendMessage={sendMessage}
+          messages={messages}
+          closeConnection={closeConnection}
+          users={users}
+        />
+      )}
+    </div>
+  );
+};
 
 export default MainChat;
