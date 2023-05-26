78abbb25bcdc6262e9898c761fee517c4303396f
Erosi1 <em52473@ubt-uni.net>
2023-05-26 22:01:34 +0200

    GET/GET(ID)/PUT/DELETE(ID) methods added

--- backend/Notifications.Service/routes/notifications.js
+++ backend/Notifications.Service/routes/notifications.js
@@ -4,9 +4,7 @@ import Notification from '../models/notifications.js'
 const router= express.Router();
 router.use(express.json());
 
-router.get("/",(req,res)=>{
-    res.send("notifications")
-})
+
 router.post('/', async (req, res) => {
   try {
     const newNotification = {
@@ -21,7 +19,77 @@ router.post('/', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-
+//GET API
+router.get("/",async(req,res)=>{
+  try{
+let notification=await Notification.find();
+res.json({
+  notification:notification
+});
+  }catch(err){
+      res.status(500).json({
+          success:false,
+          message:err.message
+      })
+     
+  } 
+});
+//GET request - get a single notification
+router.get("/:id",async(req,res)=>{
+  try{
+let notification=await Notification.findOne({_id:req.params.id});
+res.json({
+  notification:notification
+});
+  }catch(err){
+      res.status(500).json({
+          success:false,
+          message:err.message
+      })
+     
+  } 
+});
+//PUT request -update a single notification
+router.put("/:id",async(req,res)=>{
+  try{
+let notification=await Notification.findOneAndUpdate(
+  {_id:req.params.id},
+  {
+  $set:{
+      description:req.body.description,
+     }
+  },
+  {upsert:true}
+);
+res.json({
+  updatedNotification:notification
+});
+  }catch(err){
+      res.status(500).json({
+          success:false,
+          message:err.message
+      })
+     
+  } 
+});
+//DELETE request-  delete a single notification
+router.delete("/:id",async(req,res)=>{
+  try {
+   let deletedNotification=await Notification.findOneAndDelete({_id:req.params.id });
+   if(deletedNotification){
+      res.json({
+          status:true,
+          message:"Successfully deleted"
+      })
+   }
+  }catch(err){
+      res.status(500).json({
+          success:false,
+          message:err.message
+      })
+     
+  } 
+})
 
 
   
