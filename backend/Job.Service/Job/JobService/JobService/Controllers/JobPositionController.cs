877fb9aed2b995bbf54e77799a8ec7815ccfcd44
Fitore <ttahirifitore@gmail.com>
2023-05-29 23:21:38 +0200

    merge fixed

---
+++ backend/Job.Service/Job/JobService/JobService/Controllers/JobPositionController.cs
@@ -0,0 +1,43 @@
+﻿using JobService.Core.Dtos.Company;
+using JobService.Core.Dtos.JobPosition;
+using JobService.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobService.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class JobPositionController: ControllerBase
+    {
+        private readonly IJobPosition _contract;
+        public JobPositionController(IJobPosition contract)
+        {
+            _contract = contract;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllJobs()
+        {
+            return Ok(await _contract.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            return Ok(await _contract.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult AddJob(JobCreateDto dto)
+        {
+            _contract.Add(dto);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteJob(int id)
+        {
+            return Ok(await _contract.Delete(id));
+        }
+    }
+}
