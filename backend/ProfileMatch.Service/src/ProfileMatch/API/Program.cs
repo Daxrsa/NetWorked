974a304ccf247ea5e8940c66c91d6037c534cfcd
Fitore <ttahirifitore@gmail.com>
2023-06-07 22:12:28 +0200

    Matching algorithm after applying to a job position
    To be done:
    - Get user and job position to perform the algorithm

--- backend/ProfileMatch.Service/src/ProfileMatch/API/Program.cs
+++ backend/ProfileMatch.Service/src/ProfileMatch/API/Program.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using Application.Services.ResultsService;
 using Domain.AutoMapperConfig;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IResultsRepo, ResultsService>();
+builder.Services.AddScoped<CalculateMatch>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
