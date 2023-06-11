856352081176038e1b37ed7b7230a5850f7ca25e
Fitore <ttahirifitore@gmail.com>
2023-06-11 19:10:48 +0200

    Nuget package to manage files in all services

---
+++ backend/File.Package/File.Package/FileService/IFileService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace File.Package.FileService
+{
+    public interface IFileService
+    {
+        public Tuple<int, string> SaveImage(IFormFile file);
+        public bool DeleteImage(string fileName);
+    }
+}
