7522fa8a0e38ff05dcaadb0d06311e40fd761628
Daorsa Hyseni <79546481+Daxrsa@users.noreply.github.com>
2023-06-14 16:03:24 +0200

    Logged in user's posts are shown in the profile page

--- frontend/src/components/Posts/UserPostList.tsx
+++ frontend/src/components/Posts/UserPostList.tsx
@@ -1,12 +1,32 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
+import { Header, List } from "semantic-ui-react";
 
 export default function UserPostList() {
-  const[posts, setPosts] = useState([]);
-  
-  useEffect(() => {
-    axios.get('http://localhost:5263/api/Post/filter-posts');
-  })
+  const [posts, setPosts] = useState([]);
 
-  return <h1>My Posts</h1>;
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+    axios
+      .get("http://localhost:5263/api/Post/filter-posts", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        setPosts(response.data);
+      });
+  }, []);
+
+  return (
+    <>
+    <Header as = 'h2' content = "My Posts"/>
+      <List>
+        {posts.map((post: any) => (
+          <List.Item key={post.id}>{post.title}</List.Item>
+        ))}
+      </List>
+    </>
+  );
 }
