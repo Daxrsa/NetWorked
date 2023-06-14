08b22be466989a0b9dbab5cc9b3db56e3558d6e9
Erosi1 <em52473@ubt-uni.net>
2023-06-14 17:19:44 +0200

    Notification service connection to RabbitMQ

--- backend/Notifications.Service/index.js
+++ backend/Notifications.Service/index.js
@@ -1,31 +1,50 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import NotificationRoute from './routes/notifications.js'
-
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import amqp from 'amqplib';
+import NotificationRoute from './routes/notifications.js';
 import cors from 'cors';
-const app= express ()
 
-dotenv.config()
+const app = express();
+
+dotenv.config();
+
 const connect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO)
-        console.log("MongoDB connected")
-    }catch(error){
-        throw error;
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('MongoDB connected');
 
-app.get("/",(req,res)=> {
-    res.send("testing")
-})
-//middlewares
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    const exchangeName = 'my-exchange';
+    const queueName = 'my-queue';
+
+    // Create exchange
+    await channel.assertExchange(exchangeName, 'direct', { durable: true });
+
+    // Create queue
+    await channel.assertQueue(queueName, { durable: true });
+
+    // Bind queue to exchange
+    await channel.bindQueue(queueName, exchangeName, '');
+
+    console.log('RabbitMQ setup completed.');
+  } catch (error) {
+    console.error('Error setting up RabbitMQ:', error);
+  }
+};
+
+app.get('/', (req, res) => {
+  res.send('testing');
+});
+
+// Middlewares
 app.use(cors());
-app.use("/notifications",NotificationRoute);
+app.use('/notifications', NotificationRoute);
 
-
-app.listen(8800,()=> {
-    connect()
-    console.log("Connected to backend!")
-})
-
+connect().then(() => {
+  app.listen(8800, () => {
+    console.log('Connected to backend!');
+  });
+});
